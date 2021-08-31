aada2a943e84a8bd0b31c6e3003b2696ee1658d2
Anna Conway <anna.conway@ohi.net>
2021-08-31 17:37:14 -0400

    b2b

--- ApexB2B/promo/index.php
+++ ApexB2B/promo/index.php
@@ -9,16 +9,16 @@
   <title>APEX Business-to-Business Portal</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="css/main.css" rel="stylesheet" type="text/css">
-  <script type="text/javascript" src="js/validation.js"></script>
+  <link href="/css/main.css" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="/js/validation.js"></script>
 </head>
     
 <section class='grid-container site-form contact'>
     <div class='grid'>
-        <form action="mail.php" id="contact-form" onsubmit="return validateForm(['account-number', 'first-name', 'last-name', 'email'])" class="form col-10 offset-1 col-lg-5 offset-7-lg has-logo" method="post">
-            <div class='logo'><a href='/index.php'><img src='images/logo.jpg' alt='Apex'></a></div>
+        <form action="http://b2b.apexfoot.com/promo/thank-you.php" id="contact-form" onsubmit="return validateForm(['account-number', 'first-name', 'last-name', 'email'])" class="form col-10 offset-1 col-lg-5 offset-7-lg has-logo" method="post">
+            <div class='logo'><a href='/index.php'><img src='/images/logo.jpg' alt='Apex'></a></div>
             <div class="grid">
-                <h1 class='col-12 text-center'>Apex Foot Health Portal Registration</h1>
+                <h1 class='col-12 text-center'>Apex Foot Promo</h1>
                 <p class='col-12'>Welcome to the Apex Family. Once your information is submitted below, you will receive an email containing your Password and training documentation within one business day. If you have any issues, please contact us at <a href="mailto:aorder@apexfoot.com">aorder@apexfoot.com</a>.</p> 
             </div>
             <fieldset>
