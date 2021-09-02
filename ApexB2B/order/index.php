cd9e449272eae399ba1a6f7e96daaf14be61c99c
Anna Conway <anna.conway@ohi.net>
2021-09-02 13:49:16 -0400

    b2b

---
+++ ApexB2B/order/index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class='no-js lt-ie9 lt-ie8 lt-ie7'> <![endif]-->
+<!--[if IE 7]>         <html class='no-js lt-ie9 lt-ie8'> <![endif]-->
+<!--[if IE 8]>         <html class='no-js lt-ie9'> <![endif]-->
+<!--[if gt IE 8]><!--> <html class='no-js'> <!--<![endif]-->
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>APEX Business-to-Business Portal</title>
+  <meta name='description' content=''>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='stylesheet' href='/css/main.css?v=2.0'>
+  <style>
+.site-form > .grid {background-size: cover; background-repeat: no-repeat;}
+.login .site-form > .grid, .main > .grid:first-child, .site-form.forgot > .grid {
+    background-image:url("/images/ApexLogos-Dark.jpg");
+    background-size: cover;
+}
+  </style>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src='https://www.googletagmanager.com/gtag/js?id=UA-120326912-7'></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-120326912-7');
+    </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KK5WJQZ');</script>
+    <!-- End Google Tag Manager -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-KK5WJQZ\"
+    height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->    
+
+</head>
+
+<body class="index">
+    <section class="grid-container main">
+        <div class="grid justify-center">
+            <div class="col-12 col-sm-10 col-md-8 menu has-logo">
+                <div class="grid justify-end">
+                    <div class="col-2 col-bleed text-right"><a class="small" href="/index.php">Return to Apex B2B</a></div>
+                </div>
+                <div class="logo"><a href="/index.php"><img src="/images/logo.jpg" alt="Apex"></a></div>
+    
+
+            <form action="https://liveformhq.com/form/35600cbd-56d8-4014-995c-4fd9984eeef9" method="POST" accept-charset="utf-8">
+            <input type="hidden" name="_utf8" value="✓">
+            <input type="hidden" value="http://b2b.apexfoot.com/promo/thank-you.php" name="_redirect" />
+            <div class="logo"><a href="/index.php"><img src="/images/logo.jpg" alt="Apex"></a></div>
+            <div class="grid">
+                <h1 class="col-12 text-center">Apex Shoe Order Form</h1>
+                <p class="col-12">Please fill out all of the information for your order below. Once complete, please click "Submit". If you have any issues or questions, please email Apex Customer Service at <a href="mailto:aorder@apexfoot.com">aorder@apexfoot.com</a> or call <a href="tel:8002522739">800-252-2739</a>.</p> 
+            </div>
+            <fieldset>
+                <div  class="grid">
+                    <div class="col-md-6">	
+                        <label for="promo-accountnumber">Apex Account Number*</label>
+                        <input type="text" name="promo-accountnumber" id="promo-accountnumber" class="field">
+                    </div>
+                    <div class="col-md-6">	
+                        <label for="promo-accountname">Company/Account Name *</label>
+                        <input type="text" name="promo-accountname" id="promo-accountname" class="field">
+                    </div>    
+                    <div class="col-md-6">	
+                        <label for="promo-practname">Practitioner Name *</label>
+                        <input type="text" name="promo-practname" id="promo-practname" class="field">
+                    </div>     
+                    <div class="col-6">
+                        <label for="promo-phone">Phone *</label>
+                        <input type="text" name="promo-phone" id="promo-phone" class="field">
+                    </div>                                                     
+                    <div class="col-6">
+                        <label for="promo-email">Email *</label>
+                        <input type="text" name="promo-email" id="promo-email" class="field">
+                    </div>
+                    <div class="col-6">
+                        <label for="promo-code">Promo Code</label>
+                        <input type="text" name="promo-code" id="promo-code" class="field">
+                    </div> 
+                    <div class="col-6">
+                        <label for="promo-email">Shipping Address *</label>
+                        <input type="text" name="promo-address" id="promo-address" class="field">
+                    </div>
+                    <div class="col-6">
+                        <label for="promo-code">Shipping City *</label>
+                        <input type="text" name="promo-city" id="promo-city" class="field">
+                    </div>   
+                    <div class="col-6">
+                        <label for="promo-email">Shipping State *</label>
+                        <input type="text" name="promo-state" id="promo-state" class="field">
+                    </div>
+                    <div class="col-6">
+                        <label for="promo-code">Shipping Zip *</label>
+                        <input type="text" name="promo-zip" id="promo-zip" class="field">
+                    </div>                                                           
+
+                    <div class="col-3">
+                        <label for="promo-patient">Patient Name&nbsp;<div style="font-size: 10px">&nbsp;</div></label>
+                        <input type="text" name="promo-patient-1" id="promo-patient-1" class="field">
+                    </div>
+                    <div class="col-2">
+                        <label for="promo-po">PO Number&nbsp;<div style="font-size: 10px">&nbsp;</div></label>    
+                        <input type="text" name="promo-po-1" id="promo-po-1" class="field">
+                    </div>
+                    <div class="col-1">
+                        <label for="promo-qty">Qty&nbsp;<div style="font-size: 10px">&nbsp;</div></label>
+                        <input type="text" name="promo-qty-1" id="promo-qty-1" class="field">
+                    </div>                    
+                    <div class="col-3">
+                        <label for="promo-style">Style/Item Number&nbsp;<div style="font-size: 10px">&nbsp;</div></label>
+                        <input type="text" name="promo-style-1" id="promo-style-1" class="field">
+                    </div>
+                    <div class="col-1">
+                        <label for="promo-gender">Gender&nbsp;<div style="font-size: 10px">(M/W)</div></label>    
+                        <input type="text" name="promo-gender-1" id="promo-gender-1" class="field">
+                    </div>
+                    <div class="col-1">
+                        <label for="promo-width">Width&nbsp;<div style="font-size: 10px">(M,W,XW)</div></label>    
+                     <input type="text" name="promo-width-1" id="promo-width-1" class="field">
+                    </div>
+                    <div class="col-1">
+                        <label for="promo-size">Size&nbsp;<div style="font-size: 10px">&nbsp;</div></label>    
+                        <input type="text" name="promo-size-1" id="promo-size-1" class="field">
+                    </div> 
+
+                    <div class="col-3">
+                        <input type="text" name="promo-patient-2" id="promo-patient-2" class="field">
+                    </div>
+                    <div class="col-2">
+                        <input type="text" name="promo-po-2" id="promo-po-2" class="field">
+                    </div>
+                    <div class="col-1">
+                        <input type="text" name="promo-qty-2" id="promo-qty-2" class="field">
+                    </div>                    
+                    <div class="col-3">
+                        <input type="text" name="promo-style-2" id="promo-style-2" class="field">
+                    </div>
+                    <div class="col-1">                        
+                        <input type="text" name="promo-gender-2" id="promo-gender-2" class="field">
+                    </div>
+                    <div class="col-1">
+                        <input type="text" name="promo-width-2" id="promo-width-2" class="field">
+                    </div>
+                    <div class="col-1">
+                        <input type="text" name="promo-size-2" id="promo-size-2" class="field">
+                    </div> 
+
+                    <div class="col-3">
+                        <input type="text" name="promo-patient-3" id="promo-patient-3" class="field">
+                    </div>
+                    <div class="col-2">
+                        <input type="text" name="promo-po-3" id="promo-po-3" class="field">
+                    </div>
+                    <div class="col-1">
+                        <input type="text" name="promo-qty-3" id="promo-qty-3" class="field">
+                    </div>                    
+                    <div class="col-3">
+                        <input type="text" name="promo-style-3" id="promo-style-3" class="field">
+                    </div>
+                    <div class="col-1">                        
+                        <input type="text" name="promo-gender-3" id="promo-gender-3" class="field">
+                    </div>
+                    <div class="col-1">
+                        <input type="text" name="promo-width-3" id="promo-width-3" class="field">
+                    </div>
+                    <div class="col-1">
+                        <input type="text" name="promo-size-3" id="promo-size-3" class="field">
+                    </div> 
+
+                    <div class="col-12">
+                        <label for="promo-notes">Notes</label>
+                        <textarea id="w3review" name="promo-notes" id="promo-notes" class="field"></textarea>
+                    </div>                         
+                    <div class="col-12">
+                        <input type="submit" value="Submit" class="button">
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+        </div>
+        </div>
+    </section>
+
+<?php 
+//require_once("/footer.php");
+?>
