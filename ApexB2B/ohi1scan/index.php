ac147b4959599816ef99695d4fc27ed414890a11
Anna Conway <anna.conway@ohi.net>
2022-06-29 15:24:56 -0400

    apex b2b cdi page

---
+++ ApexB2B/ohi1scan/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class='no-js lt-ie9 lt-ie8 lt-ie7'> <![endif]-->
+<!--[if IE 7]>         <html class='no-js lt-ie9 lt-ie8'> <![endif]-->
+<!--[if IE 8]>         <html class='no-js lt-ie9'> <![endif]-->
+<!--[if gt IE 8]><!--> <html class='no-js'> <!--<![endif]-->
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>APEX Business-to-Business Portal</title>
+  <meta name='description' content=''>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='stylesheet' href='/css/main.css?v=2.0'>
+  <style>
+.site-form > .grid {background-size: cover; background-repeat: no-repeat;}
+.login .site-form > .grid, .main > .grid:first-child, .site-form.forgot > .grid {
+    background-image:url("/images/tealbg.jpg");
+    background-size: cover;
+}
+/* mobile show and hide */
+.content-desktop {display: block;}
+.content-mobile {display: none;}
+@media screen and (max-width: 768px) {
+.content-desktop {display: none;}
+.content-mobile {display: block;}
+}
+  </style>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src='https://www.googletagmanager.com/gtag/js?id=UA-120326912-7'></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-120326912-7');
+    </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KK5WJQZ');</script>
+    <!-- End Google Tag Manager -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-KK5WJQZ\"
+    height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->    
+
+</head>
+
+<body class="index">
+    <section class="grid-container main">
+        <div class="grid justify-center">
+            <div class="col-12 col-sm-10 col-md-8 menu has-logo">
+                <div class="logo"><a href="/index.php"><img src="/images/logo.jpg" alt="Apex"></a></div>
+                <!-- content-->
+                <p class="subheads" style="margin:10px;font-weight:bold;text-align:center;font-size: 1.5rem;">Cloud-Based Scanning 
+Technology at its Best!<br><br>
+                <iframe style="text-align:center" title="OHI1Scan Video" style="border:solid 1px #000000;" src="https://player.vimeo.com/video/346229601?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=false" 
+                                        width="680" 
+                                        height="381" 
+                                        frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></p>
+                                     
+                     <p class="subheads" style="margin:10px;text-align:center;font-weight:bold;">Use OHI1Scan to Bundle CDIs and Footwear</p>
+
+                    <p style="margin:10px">Ohi1Scan provides cloud-based technology for all your scanning needs. Cloud-based technology provides the flexibility of transitioning between the iPad and 
+your office computer seamlessly. Choose to use the Web App on your office 
+computer to set up patients OR do it on the spot in the exam room. Capture 
+CDI scans quickly and easily using the iPad + Structure Sensor Scanner. Scans 
+appear on the Web App and on the iPad ready to be attached to your order 
+form. Track the progress of your orders at a glance through the dashboard on 
+your computer. After completion, you will receive the same high-quality custom 
+diabetic inserts you've come to expect from Apex. Footwear and CDI orders will 
+be bundled and shipped together</p>
+<p style="margin:10px">All you need to get started is a Structure Sensor to be used along with your iPad. 
+Use your hardware to download our cloud-based App, and you're all set!</p>
+<p class="subheads" style="margin:10px; font-weight:bold;text-align:center;">CALL US TODAY TO GET STARTED! 800-252-2739</p>
+                
+            <div class="col-12">
+                <a href="/pdfs/Apex-OHI1Scan-CDI-2022.pdf" target="_blank" title="Apex Ohi1Scan Info Sheet" style="padding: 14px 20px;background: #571473;color: #fff;border-radius: 8px;border-bottom: 0; margin: 0 auto;display: block;width: 200px;font-weight: 400;text-align: center;text-decoration: none;">Apex Ohi1Scan Info Sheet</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/pdfs/Apex_OHI1Scan_Tips.pdf" target="_blank" title="Apex Ohi1Scan Scan Tips" style="padding: 14px 20px;background: #571473;color: #fff;border-radius: 8px;border-bottom: 0; margin: 0 auto;display: block;width: 200px;font-weight: 400;text-align: center;text-decoration: none;">Apex Ohi1Scan Scan Tips</a>
+            </div>
+<!-- content-->
+                </div>
+            </fieldset>
+            <div class="grid">
+                 <p class="col-12"> If you have any issues or questions, please email Apex Customer Service at <a href="mailto:aorder@apexfoot.com">aorder@apexfoot.com</a>.</p> 
+            </div>
+        </form>
+        </div>
+        </div>
+    </section>
+
+<?php 
+//require_once("/footer.php");
+?>
+
